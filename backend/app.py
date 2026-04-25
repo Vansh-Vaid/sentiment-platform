71ccd3eec8725576bc925969fb4ad6a6f89abf56
Vansh-Vaid <vanshvaid@hotmail.com>
2026-04-25 23:17:39 +0530

    fix model working properly

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,29 @@
-from transformers import pipeline
+from fastapi import FastAPI
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+app = FastAPI()
+
+# Load lightweight model properly
+model_name = "sshleifer/tiny-distilbert-base-uncased-finetuned-sst-2-english"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
 sentiment_pipeline = pipeline(
     "sentiment-analysis",
-    model="sshleifer/tiny-distilbert-base-uncased-finetuned-sst-2-english"
+    model=model,
+    tokenizer=tokenizer
 )
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+@app.get("/analyze")
+def analyze(text: str):
+    result = sentiment_pipeline(text)[0]
+
+    return {
+        "sentiment": result["label"],
+        "score": result["score"]
+    }
