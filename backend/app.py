7183a12830834a87624e820670aa5e134e366fe6
Vansh-Vaid <vanshvaid@hotmail.com>
2026-04-25 22:57:19 +0530

    using lightweight model

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,6 @@
-from fastapi import FastAPI
 from transformers import pipeline
 
-app = FastAPI()
-
-# Load model from HuggingFace (NO local files needed)
-sentiment_pipeline = pipeline("sentiment-analysis")
-
-@app.get("/")
-def home():
-    return {"message": "API is running"}
-
-@app.get("/analyze")
-def analyze(text: str):
-    result = sentiment_pipeline(text)[0]
-
-    return {
-        "sentiment": result["label"],
-        "score": result["score"]
-    }
+sentiment_pipeline = pipeline(
+    "sentiment-analysis",
+    model="sshleifer/tiny-distilbert-base-uncased-finetuned-sst-2-english"
+)
